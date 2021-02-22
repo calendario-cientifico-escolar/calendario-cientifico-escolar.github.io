1eb67a11f56b3df9d901aa063c01833dd9917832
Jorge Aguilera <jorge.aguilera@puravida-software.com>
2021-02-22 11:21:41 +0100

    add event year

--- scripts/api.groovy
+++ scripts/api.groovy
@@ -24,7 +24,7 @@ if( args.length != 2){
             def year = fields[2] as int
             def month = fields[1] as int
             def day = fields[0] as int     
-
+            def eventYear = lang != 'eus' ? title[-4..-1] : title[0..3]
             def file = new File("${args[1]}/${lang}/$year/$month/${day}.json")
             file.parentFile.mkdirs()
             file.text = JsonOutput.prettyPrint(JsonOutput.toJson([
@@ -34,6 +34,7 @@ if( args.length != 2){
                 day:day,
                 title:title,
                 body:body,
+                eventYear:eventYear,
 		        image:"https://calendario-cientifico-escolar.github.io/images/personajes/${fields[3]}.png"
             ]))
 
@@ -46,6 +47,7 @@ if( args.length != 2){
                 day:day,
                 title:title,
                 body:body,
+                eventYear:eventYear,
 		        image:"https://calendario-cientifico-escolar.github.io/images/personajes/${fields[3]}.png"
             ]))
         }
